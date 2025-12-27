037b2e0ad99a936a4a89eed5dedd4b1b9f3605cd
osboxes.org <brunocsd@gmail.com>
2025-12-27 16:31:57 +0100

    aktualizacja movement z upstreami zachowanie wlasnym plikow ze zmianami

--- movement.h
+++ movement.h
@@ -51,9 +51,9 @@ typedef struct {
 // This allows these preferences to be stored before entering BACKUP mode and and restored after waking from reset.
 
 typedef enum {
-    MOVEMENT_CLOCK_MODE_12H,    /// use 12 hour clock
+    MOVEMENT_CLOCK_MODE_12H = 0,    /// use 12 hour clock
     MOVEMENT_CLOCK_MODE_24H,        /// use 24 hour clock
-    MOVEMENT_CLOCK_MODE_024H = true,       /// use 24 hour clock with leading zero
+    MOVEMENT_CLOCK_MODE_024H,       /// use 24 hour clock with leading zero
     MOVEMENT_NUM_CLOCK_MODES
 } movement_clock_mode_t;
 
@@ -129,19 +129,19 @@ typedef enum {
     EVENT_LIGHT_LONG_PRESS,     // The light button was held for over half a second, but not yet released.
     EVENT_LIGHT_LONG_UP,        // The light button was held for over half a second, and released.
     EVENT_LIGHT_REALLY_LONG_PRESS, // The light button was held for more than 1.5 second, note yet released.
-    EVENT_LIGHT_REALLY_LONG_UP, // The light button was held for more than 1.5 second, and released.
+    // EVENT_LIGHT_REALLY_LONG_UP, // The light button was held for more than 1.5 second, and released.
     EVENT_MODE_BUTTON_DOWN,     // The mode button has been pressed, but not yet released.
     EVENT_MODE_BUTTON_UP,       // The mode button was pressed for less than half a second, and released.
     EVENT_MODE_LONG_PRESS,      // The mode button was held for over half a second, but not yet released.
     EVENT_MODE_LONG_UP,         // The mode button was held for over half a second, and released. NOTE: your watch face will resign immediately after receiving this event.
     EVENT_MODE_REALLY_LONG_PRESS, // The mode button was held for more than 1.5 second, note yet released.
-    EVENT_MODE_REALLY_LONG_UP, // The mode button was held for more than 1.5 second, and released.
+    // EVENT_MODE_REALLY_LONG_UP, // The mode button was held for more than 1.5 second, and released.
     EVENT_ALARM_BUTTON_DOWN,    // The alarm button has been pressed, but not yet released.
     EVENT_ALARM_BUTTON_UP,      // The alarm button was pressed for less than half a second, and released.
     EVENT_ALARM_LONG_PRESS,     // The alarm button was held for over half a second, but not yet released.
     EVENT_ALARM_LONG_UP,        // The alarm button was held for over half a second, and released.
     EVENT_ALARM_REALLY_LONG_PRESS, // The alarm button was held for more than 1.5 second, note yet released.
-    EVENT_ALARM_REALLY_LONG_UP, // The alarm button was held for more than 1.5 second, and released.
+    // EVENT_ALARM_REALLY_LONG_UP, // The alarm button was held for more than 1.5 second, and released.
 
     EVENT_ACCELEROMETER_WAKE,   // The accelerometer has detected motion and woken up.
     EVENT_SINGLE_TAP,           // Accelerometer detected a single tap. This event is not yet implemented.
--- watch-library/shared/watch/watch_common_display.c
+++ watch-library/shared/watch/watch_common_display.c
@@ -169,8 +169,7 @@ void watch_display_string(const char *string, uint8_t position) {
 }
 
 void watch_display_text(watch_position_t location, const char *string) {
-       if(!string[0]) return; // empty string, nothing to do
-       switch (location) {
+    switch (location) {
         case WATCH_POSITION_TOP:
         case WATCH_POSITION_TOP_LEFT:
             watch_display_character(string[0], 0);
